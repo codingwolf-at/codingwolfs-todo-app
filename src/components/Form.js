fb8510c94530ae449638810dd9b4afe6f6a91ce9
codingwolf-at <atameshwari9@gmail.com>
2021-01-16 18:58:46 +0530

    wiring the text input

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,27 @@
 import React from 'react';
 
-const Form = () => {
+const Form = ({inputText, setInputText, todos, setTodos}) => {
+
+    const inputTextHandler = (e) => {
+        setInputText(e.target.value);
+    }
+
+    const submitTodoHandler = (e) => {
+        e.preventDefault();
+        setTodos([
+            ...todos, {
+                text: inputText,
+                completed: false,
+                id: Math.random() * 1000 
+            }
+        ]);
+        setInputText("");
+    }
+
     return (
         <form>
-            <input type="text" className="todo-input" />
-            <button className="todo-button" type="submit">
+            <input onChange={inputTextHandler} value={inputText} type="text" className="todo-input" />
+            <button onClick={submitTodoHandler} className="todo-button" type="submit">
                 <i className="fas fa-plus-square"></i>
             </button>
             <div className="select">
