462246345f451f779906f67483b6fd3c66178bde
codingwolf-at <atameshwari9@gmail.com>
2021-01-16 15:41:20 +0530

    style : input outline

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
+// Importing components
+import Form from './components/Form'; 
 
 function App() {
   return (
     <div className="App">
-        <header>
-          <h1>codingWolf's TODO List</h1>
-        </header>
+      <header>
+        <h1>codingWolf's TODO List</h1>
+      </header>
+      <Form />
     </div>
   );
 }
